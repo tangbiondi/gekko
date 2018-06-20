0c9d0db5a1366be1fabaf58b18a42737f9dedae1
tangbiondi <biondi_t@hotmail.com>
2018-06-20 13:39:31 +0800

    m

---
+++ talibtest.js
@@ -0,0 +1,8 @@
+var talib = require('talib');
+console.log("TALib Version: " + talib.version);
+ 
+// Display all available indicator function names
+var functions = talib.functions;
+for (i in functions) {
+    console.log(functions[i].name);
+}
