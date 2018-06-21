f597c7b17b52f639056ef978638c394a0cbdf63d
tangbiondi <biondi_t@hotmail.com>
2018-06-21 15:53:52 +0800

    m

--- plugins/postgresql/handle.js
+++ plugins/postgresql/handle.js
@@ -36,7 +36,9 @@ var connectionString = config.postgresql.connectionString;
 console.log('connectionString: ' + connectionString);
 console.log('dbname: ' + dbName);
 var checkClient = new pg.Client(connectionString);
-var client = new pg.Client(config.postgresql.connectionString+"/"+dbName);
+// var client = new pg.Client(config.postgresql.connectionString+"/"+dbName);
+var client = new pg.Client(config.postgresql.connectionString);
+
 
 /* Postgres does not have 'create database if not exists' so we need to check if the db exists first.
 This requires connecting to the default postgres database first. Your postgres user will need appropriate rights. */
